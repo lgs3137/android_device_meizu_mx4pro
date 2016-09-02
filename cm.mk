e50ec740a059140a41f2a792d1d6451325b2faf7
lgs3137 <lgs3137>
2016-09-02 21:37:08 +0800

    test

--- cm.mk
+++ cm.mk
@@ -5,14 +5,14 @@ $(call inherit-product, device/meizu/mx4pro/device-common.mk)
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit some common MKOEE stuff.
-$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit nfc enhanced configuration
-$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := mk_mx4pro
+PRODUCT_NAME := cm_mx4pro
 PRODUCT_DEVICE := mx4pro
 PRODUCT_BRAND := meizu
 PRODUCT_MODEL := MX4 Pro
